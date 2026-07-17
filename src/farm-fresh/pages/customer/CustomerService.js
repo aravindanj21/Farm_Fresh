58d606b1658022048e9be9703cd6689d9722a38e
aravindanj21 <aravindanme03@gmail.com>
2026-07-17 12:43:00 +0530

    Fourth Commit

--- src/farm-fresh/pages/customer/CustomerService.js
+++ src/farm-fresh/pages/customer/CustomerService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://farmfresh-production-6db2.up.railway.app/api/customer";
-
+const API = "https://farmfresh-production-6db2.up.railway.app/api/customer";
 
 
 export const registerCustomer = async (data) => {
@@ -9,31 +8,35 @@ export const registerCustomer = async (data) => {
   return response.data;
 };
 
+
 export const loginCustomer = async (data) => {
   const response = await axios.post(`${API}/login`, data);
   return response.data;
 };
+
 
 export const sendOTP = async (data) => {
   const response = await axios.post(`${API}/send-otp`, data);
   return response.data;
 };
 
+
 export const verifyOTP = async (data) => {
   const response = await axios.post(`${API}/verify-otp`, data);
   return response.data;
 };
+
 
 export const sendForgotOTP = async (data) => {
   const response = await axios.post(`${API}/forgot-password`, data);
   return response.data;
 };
 
+
 export const resetPassword = async (data) => {
   const response = await axios.post(`${API}/reset-password`, data);
   return response.data;
 };
-
 
 
 export const searchProducts = async (keyword) => {
